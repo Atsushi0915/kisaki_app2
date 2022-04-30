3655e2d2a07a58e7052cb593bcad552a0794f586
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-04-30 22:35:46 +0900

    controller model 作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+  def top
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'posts/top'
+  get 'posts/new'
+  get 'posts/show'
+  get 'posts/index'
+  get 'posts/edit'
+  get 'users/new'
+  get 'users/show'
+  get 'users/index'
+  get 'users/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
