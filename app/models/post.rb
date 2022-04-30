9adbb8fa7e35f700a20daeda9cd1d6b46554542f
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-04-30 22:53:03 +0900

    model 修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
-class Post < ApplicationRecord
+class Post < ApplicationRecor
+  before_validation :set_nameless_name
+
+  validates :title, presence: true, length: {maximum: 40}
+  validates :content, presence: true, length: {maximum: 300}
+  validates :user_id, presence: true
+  validates :post_image, presence: true
+
+  belongs_to :user
+  has_many :comments
+
+  private
+    def set_nameless_title
+      self.title = "No Title" if title.blank?
+    end
 end
