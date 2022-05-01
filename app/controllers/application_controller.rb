16a1df24acd1701d02748d777d2fbe5a4c8d9a6b
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-05-01 21:18:43 +0900

    application controller/html.erb 作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
+  before_action :set_current_user
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,19 @@
 module SessionsHelper
+  def set_current_user
+    if session[:user_id]
+      @current_user = User.find_by(id: session[:id]) 
+    end
+  end
+
+  def not_login_user
+    if @current_user == nil
+      redirect_to login_form_path, notice: "ログインが必要です。"
+    end
+  end
+
+  def login_user
+    if @current_user
+      redirect_to root_path, notice: "既にログインしています。"
+    end
+  end
 end
